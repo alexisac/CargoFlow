6d82489839a677d3f0e38362da53af3ecc7acfd1
AlexIsac <alex_isac72@yahoo.com>
2026-04-30 11:31:41 +0300

    Initial setup for backend

---
+++ backendCargoFlow/src/main/java/com/example/backendcargoflow/BackendCargoFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendcargoflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCargoFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCargoFlowApplication.class, args);
+    }
+
+}
---
+++ backendCargoFlow/src/test/java/com/example/backendcargoflow/BackendCargoFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendcargoflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCargoFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
