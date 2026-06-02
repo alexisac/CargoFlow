c410446baa57ddeaa0a4a6e03e6104a81a557f5d
AlexIsac <alex_isac72@yahoo.com>
2026-06-02 17:29:05 +0300

    BACKEND: - Refactor backend from monolith to microservices with Eureka and API Gateway

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,17 +1,14 @@
-package com.example.backendcargoflow;
+package com.example.apigateway;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-@EnableDiscoveryClient
-public class BackendCargoFlowApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendCargoFlowApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
